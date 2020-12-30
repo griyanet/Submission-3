de6295c6f580b23c2d56107c50199102eaa96243
Trisna Widia <griyanetcafe@gmail.com>
2020-12-30 11:30:29 -0800

    first commit content-provider branch

--- app/src/main/java/com/example/submission3/alarm/AlarmActivity.kt
+++ app/src/main/java/com/example/submission3/alarm/AlarmActivity.kt
@@ -8,13 +8,7 @@ import com.example.submission3.databinding.ActivityAlarmBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
-class AlarmActivity : AppCompatActivity(), View.OnClickListener,
-    DatePickerFragment.DialogDateListener, TimePickerFragment.DialogTimeListener {
-
-    companion object {
-        private const val DATE_PICKER_TAG = "DatePicker"
-        private const val TIME_PICKER_ONCE_TAG = "TimePickerOnce"
-    }
+class AlarmActivity : AppCompatActivity() {
 
     private var binding: ActivityAlarmBinding? = null
     private lateinit var alarmReceiver: AlarmReceiver
@@ -25,56 +19,12 @@ class AlarmActivity : AppCompatActivity(), View.OnClickListener,
         binding = ActivityAlarmBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        binding?.btnOnceDate?.setOnClickListener(this)
-        binding?.btnOnceTime?.setOnClickListener(this)
-        binding?.btnSetOnceAlarm?.setOnClickListener(this)
-
         alarmReceiver = AlarmReceiver()
-    }
-
-
-    override fun onClick(v: View?) {
-        if (v != null) {
-            when (v.id) {
-                R.id.btn_once_date -> {
-                    val datePickerFragment = DatePickerFragment()
-                    datePickerFragment.show(supportFragmentManager, DATE_PICKER_TAG)
-                }
-                R.id.btn_once_time -> {
-                    val timePickerFragmentOnce = TimePickerFragment()
-                    timePickerFragmentOnce.show(supportFragmentManager, TIME_PICKER_ONCE_TAG)
-                }
-                R.id.btn_set_once_alarm -> {
-                    val onceDate = binding?.tvOnceDate?.text.toString()
-                    val onceTime = binding?.tvOnceTime?.text.toString()
-                    val onceMessage = binding?.edtOnceMessage?.text.toString()
-
-                    alarmReceiver.setOneTimeAlarm(
-                        this, AlarmReceiver.TYPE_ALARM,
-                        onceDate,
-                        onceTime,
-                        onceMessage
-                    )
-                }
-            }
+        val message = "This alarm set at 9am"
+        binding?.btnSetOnceAlarm?.setOnClickListener {
+            alarmReceiver.setOneTimeAlarm(
+                this, AlarmReceiver.TYPE_ALARM, message)
         }
-    }
-
-    override fun onDialogDateSet(tag: String?, year: Int, month: Int, dayOfMonth: Int) {
-        val calendar = Calendar.getInstance()
-        calendar.set(year, month, dayOfMonth)
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-
-        binding?.tvOnceDate?.text = dateFormat.format(calendar.time)
-    }
-
-    override fun onDialogTimeSet(tag: String?, hourOfDay: Int, minute: Int) {
-        val calendar = Calendar.getInstance()
-        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
-        calendar.set(Calendar.MINUTE, minute)
-        val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
-
-        binding?.tvOnceTime?.text = dateFormat.format(calendar.time)
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/submission3/alarm/ScheduleAlarmReceiver.kt
+++ app/src/main/java/com/example/submission3/alarm/ScheduleAlarmReceiver.kt
@@ -13,29 +13,29 @@ import android.util.Log
 import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
-import com.example.submission3.MainActivity
 import com.example.submission3.R
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
-class AlarmReceiver : BroadcastReceiver() {
+class ScheduleAlarmReceiver: BroadcastReceiver()  {
 
     companion object {
         const val TYPE_ALARM = "Notification Alarm to visit Activity"
         const val EXTRA_MESSAGE = "message"
         const val EXTRA_TYPE = "type"
-        const val ALARM_ID = 102
+        private const val ALARM_ID = 102
         private const val DATE_FORMAT = "yyyy-MM-dd"
         private const val TIME_FORMAT = "HH:mm"
+        private const val ID_ONETIME = 100
     }
 
-    override fun onReceive(context: Context, intent: Intent) {
-        val type = intent.getStringExtra(EXTRA_TYPE)
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
+    override fun onReceive(context: Context, intent: Intent?) {
+        val type = intent?.getStringExtra(ScheduleAlarmReceiver.EXTRA_TYPE)
+        val message = intent?.getStringExtra(ScheduleAlarmReceiver.EXTRA_MESSAGE)
 
-        val title = TYPE_ALARM
-        val notifyId = ALARM_ID
+        val title = AlarmReceiver.TYPE_ALARM
+        val notifyId = AlarmReceiver.ALARM_ID
 
         showToast(context, title, message)
 
@@ -51,26 +51,40 @@ class AlarmReceiver : BroadcastReceiver() {
     fun setOneTimeAlarm(
         context: Context,
         type: String,
+        date: String,
+        time: String,
         message: String
     ) {
-
+        if (isDateInvalid(date, DATE_FORMAT) || isDateInvalid(time, TIME_FORMAT)) return
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val intent = Intent(context, AlarmReceiver::class.java).let { intent ->
-            intent.putExtra(EXTRA_MESSAGE, message)
-            intent.putExtra(EXTRA_TYPE, type)
-            PendingIntent.getBroadcast(context, ALARM_ID, intent, 0)
-        }
-        val calendar: Calendar = Calendar.getInstance().apply {
-            timeInMillis = System.currentTimeMillis()
-            set(Calendar.HOUR_OF_DAY, 9)
-        }
-        alarmManager.set(
-            AlarmManager.RTC_WAKEUP,
-            calendar.timeInMillis,
-            intent
-        )
+        val intent = Intent(context, ScheduleAlarmReceiver::class.java)
+        intent.putExtra(EXTRA_MESSAGE, message)
+        intent.putExtra(EXTRA_TYPE, type)
+        Log.e("ONE TIME", "$date $time")
+        val dateArray = date.split("-").toTypedArray()
+        val timeArray = time.split(":").toTypedArray()
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.YEAR, Integer.parseInt(dateArray[0]))
+        calendar.set(Calendar.MONTH, Integer.parseInt(dateArray[1]) - 1)
+        calendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dateArray[2]))
+        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]))
+        calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]))
+        calendar.set(Calendar.SECOND, 0)
+        val pendingIntent = PendingIntent.getBroadcast(context, ID_ONETIME, intent, 0)
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
 
         Toast.makeText(context, "One time alarm set up", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isDateInvalid(date: String, format: String): Boolean {
+        return try {
+            val df = SimpleDateFormat(format, Locale.getDefault())
+            df.isLenient = false
+            df.parse(date)
+            false
+        } catch (e: ParseException) {
+            true
+        }
     }
 
     private fun showAlarmNotification(
@@ -87,7 +101,7 @@ class AlarmReceiver : BroadcastReceiver() {
             context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
-        val intent = Intent(context, MainActivity::class.java)
+        val intent = Intent(context, ScheduleAlarmActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
         val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
 
