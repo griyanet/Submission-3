37dfceb71ddc7ae1bbe8a8400945569de62d2281
Trisna Widia <griyanetcafe@gmail.com>
2020-12-31 11:00:34 +0800

    Update Constant.kt

--- app/src/main/java/com/example/submission3/api/Constant.kt
+++ app/src/main/java/com/example/submission3/api/Constant.kt
@@ -3,6 +3,6 @@ package com.example.submission3.api
 class Constant {
     companion object {
         const val BASE_URL = "https://api.github.com/"
-        const val TOKEN = "b4b516d5bb356c994c6a802f55e9b7928c90d364"
+        const val TOKEN = ""
     }
 }
